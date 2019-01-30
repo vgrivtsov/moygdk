413c66759231e6f36116c9530137628da92a5326
vgrivtsov <vgrivtsov@gmail.com>
2019-01-30 05:19:49 +0300

    ff

--- gdksite/settings/production.py
+++ gdksite/settings/production.py
@@ -8,5 +8,14 @@ except ImportError:
     pass
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gdksite',
+        'PASSWORD': 'vic83890',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 ALLOWED_HOSTS = ['95.213.236.166', 'moygdk.ru', 'www.moygdk.ru']
